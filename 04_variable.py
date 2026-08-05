183c306caf39a5cc8a95bf7a2f71bddc1f9abc4f
suemin0624 <suemin0624@gmail.com>
2026-08-05 09:43:17 +0900

    04_variable 실습

--- 04_variable.py
+++ 04_variable.py
@@ -9,8 +9,8 @@
 # temp = 36  # 숫자로 저장하고 싶다면 따옴표 금지
 name = "센서 A"  # 글자는 무조건 따옴표로 감싸기
 
-print(temp)  # 36
-print("temp")  # temp
+# print(temp)  # 36
+# print("temp")  # temp
 
 # =은 "같다"가 아니라 "저장"하는 것
 # 비교는 ==을 사용
@@ -22,18 +22,18 @@ x = 5
 x = x + 5
 # 변수를 "재활용"할 때 변수 기존 자신의 값을 활용할 수 있음
 # 변수에 할당할 때 오른쪽을 먼저 계산한 뒤 x에 값을 재할당
-print(x)  # 10
+# print(x)  # 10
 
 # y = y + 5  # 오류 발생 (y가 선언되지 않았기 때문)
 
 # ====================================
 print("==== 재할당 =====")
 
-print("재할당하기 전 temp:", temp)
-# temp = 15  # 위에서 할당했던 36이라는 값을 15로 재할당(수정)
-Temp = 15  # temp와 다른 변수로 동작
-print("temp:", temp)
-print("TempL", Temp)
+# print("재할당하기 전 temp:", temp)
+# # temp = 15  # 위에서 할당했던 36이라는 값을 15로 재할당(수정)
+# Temp = 15  # temp와 다른 변수로 동작
+# print("temp:", temp)
+# print("TempL", Temp)
 
 # 재할당은 지금까지 실행된 가장 마지막으로 저장된 값을 불러옴
 # print(score)  # NameError 발생
@@ -66,16 +66,16 @@ print("====== 주석으로 변수 설명 ======")
 # temp = 25  # 온도(섭씨)
 count = 3  # 센서 개수
 # temp = 1000000000 # 주석처리된 코드는 동작하지 않음
-print(temp)  # 25
+# print(temp)  # 25
 
-# 실습 1
+"""실습 1"""
 # temp = 25
 # print(temp)
 
 name = "센서A"
 print(name)
 
-# 실습 2
+"""실습 2"""
 # temp = 25
 # print(temp)
 # temp = 30
@@ -86,20 +86,20 @@ print(temperature)
 name = "센서A"
 print(name)
 
-# 실습 3
+"""실습 3"""
 my_number = 624
 print(my_number)
 
 mood = "그냥 그럼"
 print(mood)
 
-# 실습 4
+"""실습 4"""
 age = 24
 label = "나이"
 print(label)
 print(age)
 
-# 실습 5
+"""실습 5"""
 x = 10
 print(x)
 
@@ -109,18 +109,18 @@ print(x)
 x = x * 2
 print(x)
 
-# 실습 6
+"""실습 6"""
 a = 100
 b = a
 a = 999
 print(a)
 print(b)
 
-# 실습 7
+"""실습 7"""
 # (오류 발생)
-print(temp)
-score = 90
-print(Score)
+# print(temp)
+# score = 90
+# print(Score)
 
 # (오류 고치기)
 temp = 25
@@ -128,33 +128,33 @@ print(temp)
 score = 90
 print(score)
 
-# 실습 8
-temp = 25 # 온도(섭씨)
-count = 3 # 센서 개수
+"""실습 8"""
+temp = 25  # 온도(섭씨)
+count = 3  # 센서 개수
 # temp = 100  # 실행 안 됨
-print(temp) # 25
+print(temp)  # 25
 
-# 실습 9
+"""실습 9"""
 x = 5
 x = 10
 x = x + 1
-print(x) # 11
+print(x)  # 11
 
-# 실습 10
+"""실습 10"""
 name = "이수민"
 temp = 38
 print("이름")
 print(name)
 print(temp)
 
-# 실습 11
+"""실습 11"""
 a = 24
 age = 24
 device_temp = 34
 print(age)
 print(device_temp)
 
-# 실습 12
+"""실습 12"""
 
 # 한 줄에 변수 세 개 만들기
 x, y, z = 14, 54, 33
